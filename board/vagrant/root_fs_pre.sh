012a9250ffd3aff7ed5616c84d844a10c61cf202
vagrant <vagrant@precise64.(none)>
2013-08-02 06:01:12 +0000

    use a custom skeleton

--- board/vagrant/root_fs_pre.sh
+++ board/vagrant/root_fs_pre.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-DIR=$1
-
-mv $DIR/etc/inittab $DIR/etc/inittab.bak
-sed 's/getty/getting -a root/' $DIR/etc/inittab.bak > $DIR/etc/inittab
-rm $DIR/etc/inittab.bak
-
-grep vagrant $DIR/etc/passwd || echo 'vagrant:x:1001:1001:Linux User,,,:/home/vagrant:/bin/ash' >> $DIR/etc/passwd
+TARGET_DIR=$1
